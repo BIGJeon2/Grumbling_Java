02c43ece1644d82a60fb555a93da0304941dbf20
Big_Jeon <robert6728@naver.com>
2021-05-30 16:43:47 +0900

    Setting_package_name

--- app/src/main/java/com/big_jeon/grumbling/MainActivity.java
+++ app/src/main/java/com/big_jeon/grumbling/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.grumbling;
+package com.big_jeon.grumbling;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.grumbling.R;
 
 public class MainActivity extends AppCompatActivity {
 
